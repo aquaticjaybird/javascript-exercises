64b705fb5200d84a6d9d6ecbcf4a4d1ad729e5b9
aquaticjaybird <aquaticjaybird@gmail.com>
2025-11-21 16:04:24 +0000

    Complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    let repeatedStr = '';
+    for (let i = 0; i < repeats; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
