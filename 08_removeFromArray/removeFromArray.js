6cc1d7a1ec9f863a064654401275bcd045d77963
aquaticjaybird <aquaticjaybird@gmail.com>
2025-11-21 16:50:27 +0000

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    for (let i = 0; i < arr.length; i++) {
+        if (itemsToRemove.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
