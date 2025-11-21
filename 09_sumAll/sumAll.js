c9140a576f6a08bcfc54bbf6086d5f9dc073b6b7
aquaticjaybird <aquaticjaybird@gmail.com>
2025-11-21 22:00:53 +0000

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if ( !(Number.isInteger(int1) && Number.isInteger(int2)) ) {
+        return "ERROR";
+    }
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+    const sequenceLength = Math.abs(int1 - int2) + 1;
+    return (int1 + int2) * sequenceLength / 2;
 };
 
 // Do not edit below this line
