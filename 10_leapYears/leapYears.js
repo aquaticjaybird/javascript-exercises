3feb5cd4003c24d30a34bd07613f26b5b7020626
aquaticjaybird <aquaticjaybird@gmail.com>
2025-11-21 22:45:44 +0000

    Complete exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisibleBy4 = year % 4 === 0;
+    const divisibleBy100 = year % 100 === 0;
+    const divisibleBy400 = year % 400 === 0;
+    if (!divisibleBy4) {
+        return false;
+    }
+    if (!divisibleBy100) {
+        return true;
+    }
+    if (divisibleBy400) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
