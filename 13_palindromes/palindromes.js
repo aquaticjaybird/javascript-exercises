1c1cf6d5494068358aa1af9b96404b671df3c69b
aquaticjaybird <aquaticjaybird@gmail.com>
2025-12-02 01:23:01 +0000

    Complete 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanStr = str.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let posStart = 0;
+    let posEnd = cleanStr.length - 1;
+    while (posStart < cleanStr.length / 2) {
+        if (cleanStr[posStart] !== cleanStr[posEnd]) {
+            return false;
+        }
+        posStart++;
+        posEnd--;
+    }
+    return true;
 };
 
 // Do not edit below this line
