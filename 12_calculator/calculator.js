7023bc0ed009a9b8cb0a369905bf469f7736fb17
aquaticjaybird <aquaticjaybird@gmail.com>
2025-12-02 00:48:30 +0000

    Complete 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, item, index, arr) => {
+    if (arr[index + 1]) {
+      product *= arr[index + 1];
+    }
+    return product;
+  }, arr[0]);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
