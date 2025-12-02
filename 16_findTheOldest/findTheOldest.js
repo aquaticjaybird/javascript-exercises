41e93e67722cf175e3ee28d90616cf6d4bbacd5a
aquaticjaybird <aquaticjaybird@gmail.com>
2025-12-02 02:21:50 +0000

    Complete 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const peopleFromOldest = people.sort((a, b) => {
+        const aAge = a.yearOfDeath ?
+            a.yearOfDeath - a.yearOfBirth :
+            currentYear - a.yearOfBirth;
+        const bAge = b.yearOfDeath ?
+            b.yearOfDeath - b.yearOfBirth :
+            currentYear - b.yearOfBirth;
+        if (aAge > bAge) return -1;
+        if (aAge < bAge) return 1;
+        if (aAge === bAge) return 0;
+    });
+    return peopleFromOldest[0];
 };
 
 // Do not edit below this line
