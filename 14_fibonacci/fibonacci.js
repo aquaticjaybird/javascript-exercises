76d12b56aa629d7c8438f9f9c4f8031d68be1ed3
aquaticjaybird <aquaticjaybird@gmail.com>
2025-12-02 01:56:01 +0000

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberNum) {
+    let preMember = 1;
+    let curMember = 1;
+    if (memberNum == 0) return 0;
+    if (memberNum == 1) return preMember;
+    if (memberNum < 0) return "OOPS";
+    for (let i = 2; i < memberNum; i++) {
+        const nextMember = curMember + preMember;
+        preMember = curMember;
+        curMember = nextMember;
+    }
+    return curMember;
 };
 
 // Do not edit below this line
